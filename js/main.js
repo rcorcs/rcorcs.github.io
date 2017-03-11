e6bba43704602de68b0feb88660b39ce2ce7a731
Isabella Ribeiro <isabella@bakkenbaeck.no>
2017-03-11 02:01:07 +0000

    graph.js data

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var data = {
+    labels: ["2013", "2014", "2015", "2016", "2017"],
+    datasets: [{
+        label: "Publications",
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+        data: [1, 1, 3, 5, 2],
+    },
+    {
+      label: "Citations",
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+      borderWidth: 1,
+      data: [0, 0, 0, 5, 0],
+    }]
+};
+var options = {
+    responsive: false,
+    barThickness: 15,
+    scales: {
+        yAxes: [{
+            ticks: {
+                max: 6,
+                min: 0,
+                stepSize: 1,
+            },
+            stacked: false
+        }],
+    }
+};
+var ctx = document.getElementById("publicationsChart").getContext("2d");
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+});
