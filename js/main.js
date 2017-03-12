420b858816dcdabad3e4d03cf7a01f7a7f234bc9
Rodrigo Caetano Rocha <rcor.cs@gmail.com>
2017-03-12 12:46:59 +0000

    Updates colours in publication plot

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var data = {
     labels: ["2013", "2014", "2015", "2016", "2017"],
     datasets: [{
         label: "Publications",
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        backgroundColor: 'rgba(54, 162, 235, 0.4)',
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
         data: [1, 1, 3, 5, 2],
     },
     {
       label: "Citations",
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
       borderColor: 'rgba(0, 0, 0, 0.4)',
       borderWidth: 1,
       data: [0, 0, 0, 5, 0],
