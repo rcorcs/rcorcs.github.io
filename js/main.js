fc0de8985f1dfc31591ef10ad52e426d5a1b6efe
Rodrigo Caetano Rocha <rcor.cs@gmail.com>
2018-12-16 20:03:03 +0000

    adds accepted papers for 2019

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var data = {
-    labels: ["2013", "2014", "2015", "2016", "2017", "2018"],
+    labels: ["2013", "2014", "2015", "2016", "2017", "2018", "2019"],
     datasets: [{
         label: "Publications",
         backgroundColor: 'rgba(54, 162, 235, 0.4)',
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
-        data: [1, 1, 3, 5, 8, 3],
+        data: [1, 1, 3, 5, 8, 3, 2],
     },
     {
       label: "Citations",
       backgroundColor: 'rgba(0, 0, 0, 0.2)',
       borderColor: 'rgba(0, 0, 0, 0.4)',
       borderWidth: 1,
-      data: [0, 0, 0, 3, 10, 20],
+      data: [0, 0, 0, 3, 10, 20, 0],
     }]
 };
 var options = {
