61c7a66d9c6e293369b67f57c79b276c731ad554
Rodrigo Rocha <rcor@dcc.ufmg.br>
2026-04-05 16:22:22 +0100

    Create venn-data.js

---
+++ zhongwen/venn-data.js
@@ -0,0 +1,33 @@
+window.vennConfig = {
+  "categories": [
+    "skritter",
+    "DuChinese",
+    "lingqs-known"
+  ],
+  "timeline": [
+    {
+      "date": "2026-01-11",
+      "regions": {
+        "skritter_only": 1802,
+        "DuChinese_only": 838,
+        "lingqs-known_only": 1156,
+        "skritter_DuChinese_only": 515,
+        "skritter_lingqs-known_only": 628,
+        "DuChinese_lingqs-known_only": 99,
+        "skritter_DuChinese_lingqs-known": 591
+      }
+    },
+    {
+      "date": "2026-04-04",
+      "regions": {
+        "skritter_only": 1876,
+        "DuChinese_only": 837,
+        "lingqs-known_only": 1279,
+        "skritter_DuChinese_only": 490,
+        "skritter_lingqs-known_only": 676,
+        "DuChinese_lingqs-known_only": 110,
+        "skritter_DuChinese_lingqs-known": 627
+      }
+    }
+  ]
+};
